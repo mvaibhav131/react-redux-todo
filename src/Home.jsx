6df8b2a4f7a84edd82851a8433dd47aab4e94e24
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-03 14:08:16 +0530

    :bug: fixed code format

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,43 +1,84 @@
-import { Box, FormControl, FormLabel, Input,Button, Center, Heading } from '@chakra-ui/react';
-import React, { useState } from 'react'
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Center,
+  Heading,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Home = () => {
-  const [value1,setValue1]=useState('');
-  const [value2,setValue2]=useState('');
-  const [sum,setSum]=useState('');
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  const [sum, setSum] = useState("");
 
-  const value1Change=(e)=>{
+  const value1Change = (e) => {
     setValue1(e.target.value);
-  }
+  };
 
-  const value2Change=(e)=>{
+  const value2Change = (e) => {
     setValue2(e.target.value);
-  }
-  const formsSubmit=(e)=>{
+  };
+  const formsSubmit = (e) => {
     e.preventDefault();
-    const Number1=parseFloat(value1);
-    const Number2=parseFloat(value2);
-    setSum(Number1+Number2);
-  }
-  
+    const Number1 = parseFloat(value1);
+    const Number2 = parseFloat(value2);
+    setSum(Number1 + Number2);
+  };
+
   return (
     <>
-  <Heading as='h1' size='2xl' color={'green'} textAlign={'center'} ml={-7} noOfLines={1}>
-    Sum Calculator
-  </Heading>
-  <Center>
-  <Box w="35%" h="500px" fontSize={25} fontWeight="extrabold" color='blue' p={9} border={'2px solid black'}  >
-  <FormControl isRequired m={3}>
-  <FormLabel>First Value</FormLabel>
-  <Input type="number" value={value1} onChange={value1Change} placeholder='Enter First Value' />
-  <FormLabel>Second Value</FormLabel>
-  <Input type="number" value={value2} onChange={value2Change} placeholder='Enter Second Value' />
-  <Box m={6}>
-  <Button w={300} m={3} p={7} colorScheme='blue' onClick={formsSubmit} >Calculate Sum</Button>
-  </Box>
-  <Box>Sum Is : {sum}</Box>
-  </FormControl>
-      {/* <form action="" onSubmit={formSubmit}>
+      <Heading
+        as="h1"
+        size="2xl"
+        color={"green"}
+        textAlign={"center"}
+        ml={-7}
+        noOfLines={1}
+      >
+        Sum Calculator
+      </Heading>
+      <Center>
+        <Box
+          w="35%"
+          h="500px"
+          fontSize={25}
+          fontWeight="extrabold"
+          color="blue"
+          p={9}
+          border={"2px solid black"}
+        >
+          <FormControl isRequired m={3}>
+            <FormLabel>First Value</FormLabel>
+            <Input
+              type="number"
+              value={value1}
+              onChange={value1Change}
+              placeholder="Enter First Value"
+            />
+            <FormLabel>Second Value</FormLabel>
+            <Input
+              type="number"
+              value={value2}
+              onChange={value2Change}
+              placeholder="Enter Second Value"
+            />
+            <Box m={6}>
+              <Button
+                w={300}
+                m={3}
+                p={7}
+                colorScheme="blue"
+                onClick={formsSubmit}
+              >
+                Calculate Sum
+              </Button>
+            </Box>
+            <Box>Sum Is : {sum}</Box>
+          </FormControl>
+          {/* <form action="" onSubmit={formSubmit}>
         <label htmlFor="value1">Value1 : 
         <input type="number" value={value1} onChange={value1Change} placeholder='Enter Value1'/>
         </label>
@@ -50,10 +91,10 @@ const Home = () => {
         <br />
         <div><span>Sum Is : {sum}</span></div>
       </form> */}
-    </Box>
-    </Center>
+        </Box>
+      </Center>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
