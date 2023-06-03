27f1aa9ad89d0b2f05fb4bb32540114580d26b7a
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-03 14:06:36 +0530

    :airplane: provider add

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import useReducer from './useReducer';
+import { ChakraProvider } from '@chakra-ui/react'
 
-const store=configureStore({
-  reducer:{
-    users:useReducer
-  }
-})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <ChakraProvider>
+       <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
