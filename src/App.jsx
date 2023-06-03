c98ddcc5c3825314859af10a65f516a6724fb655
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-06-03 14:04:41 +0530

    :+1: adde route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import Create from './Create';
-import Update from './Update';
+
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
-      <Route path='/create' element={<Create/>}></Route>
-      <Route path='/edit/:id' element={<Update/>}></Route>
-
     </Routes>
     </BrowserRouter>
   );
